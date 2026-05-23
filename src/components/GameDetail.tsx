de9d25aeab4700b3afc3811f12e6978a2b8e7fee
Amit Naik <amitnaik0023@gmail.com>
2026-05-23 23:04:09 +0530

    style: change main media image object-fit to contain to prevent cropping and zoomed-in appearance

--- src/components/GameDetail.tsx
+++ src/components/GameDetail.tsx
@@ -475,7 +475,7 @@ export function GameDetail({ deals, isLoading }: { deals: GameDeal[], isLoading?
                       >
                         <img
                           src={selectedMediaItem.url || selectedMediaItem.thumbnail}
-                          className="w-full h-full object-cover"
+                          className="w-full h-full object-contain"
                           alt={deal.title}
                         />
                         <div className="absolute inset-0 bg-black/10 group-hover:bg-black/5 transition-colors duration-300" />
