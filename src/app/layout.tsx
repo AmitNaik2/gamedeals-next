0a89b668f63f4521bab918931b9b4fcce5abc36b
Amit Naik <amitnaik0023@gmail.com>
2026-05-23 15:27:02 +0530

    feat: add vercel speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const siteUrl = "https://www.gamesdealshub.me";
@@ -106,6 +107,7 @@ export default function RootLayout({
 
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
